c541d108981394950c9dec19a3b79520b8d00b69
skyrizer <wafir2002@gmail.com>
2023-06-22 21:57:12 +0800

    exercise 5

---
+++ Lab14Part1Exercise5/src/controller/calculator.java
@@ -0,0 +1,31 @@
+package controller;
+
+/**
+ * This is a controller class that manage the arithmetic operations.
+ * 
+ * @author wafirdzihni
+ *
+ */
+public class calculator {
+	
+	/**
+	 * A method that will get the summation from two numbers
+	 * @param number1
+	 * @param number2
+	 * @param number3
+	 * @return summation of the numbers
+	 * @return multiplication of the numbers
+	 */
+	public int numberSum(int number1, int number2, int number3) {
+		
+		return number1 + number2 + number3;
+		
+	}
+	
+	public int numberMultiplication (int number1, int number2, int number3) {
+		
+		return number1 * number2 * number3;
+		
+	}
+
+}
